837cab9f5e42e126c75e1ede624d329b62f304b8
kashpur <kashpur.info@gmail.com>
2018-08-03 13:02:12 +0300

    Доработка под 8 томкат

--- src/main/java/com/gillsoft/EncodingFilter.java
+++ src/main/java/com/gillsoft/EncodingFilter.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,6 +17,16 @@ public class EncodingFilter implements Filter {
 			throws IOException, ServletException {
 		request.setCharacterEncoding(StandardCharsets.UTF_8.name());
 		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
 	}
 
 }
