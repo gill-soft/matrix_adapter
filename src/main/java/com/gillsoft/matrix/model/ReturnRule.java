40b1e2d7a7f49b4d2f1008c0242bd1f00fc6d79d
kashpur <kashpur.info@gmail.com>
2018-12-30 20:09:56 +0200

    Добавлены новые параметры в модель условий возврата

--- src/main/java/com/gillsoft/matrix/model/ReturnRule.java
+++ src/main/java/com/gillsoft/matrix/model/ReturnRule.java
@@ -1,5 +1,6 @@
 package com.gillsoft.matrix.model;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +14,12 @@ public class ReturnRule {
 	private String title;
 	private String description;
 	private int minutesBeforeDepart;
+	private int calculationType;
+	private BigDecimal value;
+	private BigDecimal feeValue;
+	
+	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	private String activeFrom;
 	
 	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date activeTo;
@@ -39,6 +46,38 @@ public class ReturnRule {
 
 	public void setMinutesBeforeDepart(int minutesBeforeDepart) {
 		this.minutesBeforeDepart = minutesBeforeDepart;
+	}
+
+	public int getCalculationType() {
+		return calculationType;
+	}
+
+	public void setCalculationType(int calculationType) {
+		this.calculationType = calculationType;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public BigDecimal getFeeValue() {
+		return feeValue;
+	}
+
+	public void setFeeValue(BigDecimal feeValue) {
+		this.feeValue = feeValue;
+	}
+
+	public String getActiveFrom() {
+		return activeFrom;
+	}
+
+	public void setActiveFrom(String activeFrom) {
+		this.activeFrom = activeFrom;
 	}
 
 	public Date getActiveTo() {
